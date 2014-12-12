2bbd66033248f06a317d245b59589fad117e0348
nickcluc <nclucas01@gmail.com>
2014-12-12 14:25:18 -0500

    Generated scaffold for article

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework  nil, :fixture => false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
